7e1ea652bf4336230815531497d0270b60e71673
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-04-07 21:59:44 -0300

    feat: save user in memory

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import http from 'node:http'
 
 const PORT = 3333
 
+const users = []
+
 // CommomJS => require  antigo
 // ESModules => import/export
 /**
@@ -14,6 +16,23 @@ const PORT = 3333
  */
 
 const server = http.createServer((request, response ) => {
+  const { method, url } = request
+
+  if (method === 'GET' && url === '/users' ) {
+    return response
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' &&  url === '/users') {
+    users.push({
+      name: 'John Doe',
+      email: 'johndoes@example.com'
+    })
+    return response.end( 'Criação de ursers')
+  }
+
+// early return
   return response.end('Hellow World!!! mae')
 })
 
